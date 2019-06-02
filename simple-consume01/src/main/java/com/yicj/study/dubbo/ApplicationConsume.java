2173bde532a07ebb4750b3bebbaafe19d0ecd4ba
yichengjie <626659321@qq.com>
2019-06-02 21:55:44 +0800

    consume错误解决，创建spring容器的时候传入的配置文件是网络配置文件，无法访问了，所以就超时了

--- simple-consume01/src/main/java/com/yicj/study/dubbo/ApplicationConsume.java
+++ simple-consume01/src/main/java/com/yicj/study/dubbo/ApplicationConsume.java
@@ -1,31 +1,27 @@
 package com.yicj.study.dubbo;
 
-import java.util.List;
-
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.yicj.study.dubbo.entity.User;
+import com.yicj.study.dubbo.service.UserService;
 
-import com.yicj.study.api.entity.User;
-import com.yicj.study.api.service.IUserService; 
 
 public class ApplicationConsume {
 	
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"http://10.20.160.198/wiki/display/dubbo/consumer.xml"});
+		ClassPathXmlApplicationContext context = 
+			new ClassPathXmlApplicationContext("classpath:consume.xml");
         context.start();
         test1(context);
 	}
 	
-	
 	public static void test1(ClassPathXmlApplicationContext context) {
-		 IUserService userService = (IUserService)context.getBean("userService") ;
-	     List<User> list = userService.list();
-	     list.forEach(System.out::println);
-	}
-	
-	
-	public static void test2(ClassPathXmlApplicationContext context) {
-		//UserService userService = (UserService)context.getBean("userService"); // 获取远程服务代理
-		//User user = userService.queryUserById(1001) ;
+		// 获取远程服务代理
+		UserService userService = 
+				(UserService)context.getBean("userService"); 
+		User user = userService.queryUserById(1001) ;
+		System.out.println("===============================");
+		System.out.println(user);
+		System.out.println("===============================");
 	}
 	
 }
